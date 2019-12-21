0dc758cd3f17f57fbed4aeae22d9a041bec68e6f
Bruno Pastre <pastre68@gmail.com>
2019-12-21 14:15:34 -0300

    Deleted unused  files, fixed ConnectionFacade

--- spitro/networking/ConnectionFacade.swift
+++ spitro/networking/ConnectionFacade.swift
@@ -32,36 +32,15 @@ class ConnectionFacade{
         self.privateId = String.random(length: 32)
     }
     
-    func join(room named: String){
-        self.roomId = named
-        
-        NotificationCenter.default.post(name: ConnectionFacade.Notifications[.joinedGame
-            ]!, object:  nil)
-        
-    }
-    
-    func joinGame(){
-        // TODO
-    }
-    
-    
-    func updatePlayer(at position: CGPoint, with rotation: CGFloat, with speed: CGPoint){
-        let update = PlayerUpdate(id: self.privateId, position: position, zRotation: rotation, velocity: speed)
-        
-    }
-    
-    func onPlayerUpdate(_ encodedUpdate: String){
-        let update = PlayerUpdate(from: encodedUpdate)
-        
-        NotificationCenter.default.post(name: ConnectionFacade.Notifications[.playerUpdated]!, object: nil, userInfo: ["updates" : update])
-    }
-    
     func setupConnection() {
         self.connection = Connection()
-        
         self.connection.setupConnections()
         
     }
+    func disconnect(){
+       // TODO
+    }
+
     
     func updateDirection(to: Any) {
         self.connection.emit("directionChanged", message: [to])
@@ -70,11 +49,7 @@ class ConnectionFacade{
     func updateSpeed(to: Any) {
          self.connection.emit("speedChanged", message: [to])
     }
-    
-    func disconnect(){
         
-    }
-    
     func getCurrentPlayerId() -> String {
         return self.connection.getSocket().sid
     }
